bd3474634c2ea1900dac31ce7ec0bfa54524c537
parhimarsen <parhimarsen@mail.ru>
2024-07-14 17:29:29 +0300

    Fixed validation

--- DAL/Models/Post.cs
+++ DAL/Models/Post.cs
@@ -6,7 +6,6 @@ namespace DAL.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Заголовок обязателен для заполнения.")]
-        [StringLength(100, ErrorMessage = "Заголовок не должен превышать 100 символов.")]
         public string Title { get; set; }
         [Required(ErrorMessage = "Url обязателен для заполнения.")]
         public string Url { get; set; }
