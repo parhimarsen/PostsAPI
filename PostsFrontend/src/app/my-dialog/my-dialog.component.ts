c0d84b284743cdbfcaba752cd9b0acb0e68d55f9
parhimarsen <parhimarsen@mail.ru>
2024-07-14 17:32:28 +0300

    Changed frontend

--- PostsFrontend/src/app/my-dialog/my-dialog.component.ts
+++ PostsFrontend/src/app/my-dialog/my-dialog.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ApiService } from '../api.service';
-import { Post } from '../app.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-my-dialog',
   templateUrl: './my-dialog.component.html',
   styleUrls: ['./my-dialog.component.css'],
 })
-export class MyDialogComponent implements OnInit {
+export class MyDialogComponent implements OnInit, OnDestroy {
   postForm = new FormGroup({
     title: new FormControl('', [Validators.required]),
     url: new FormControl('', [Validators.required]),
   });
+  subscriptions!: Subscription;
 
   constructor(
     public dialogRef: MatDialogRef<MyDialogComponent>,
@@ -22,11 +23,16 @@ export class MyDialogComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
   onSave() {
     if (this.postForm.valid) {
       const newPost = this.postForm.value;
-      this.apiService.addPost(newPost).subscribe();
-      this.dialogRef.close(newPost);
+      this.subscriptions = this.apiService
+        .addPost(newPost)
+        .subscribe(() => this.dialogRef.close(newPost));
     }
   }
 }
